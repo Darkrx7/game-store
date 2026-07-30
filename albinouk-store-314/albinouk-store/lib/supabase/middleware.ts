b42ae0b1d1be71fd71b0974099859830bc838707
Darkrx7 <jfrmad039@gmail.com>
2026-07-30 14:19:10 +0000

    Update middleware.ts

--- albinouk-store-314/albinouk-store/lib/supabase/middleware.ts
+++ albinouk-store-314/albinouk-store/lib/supabase/middleware.ts
@@ -1,12 +1,5 @@
 import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
-import type { CookieOptions } from '@supabase/ssr'
-
-interface CookieToSet {
-  name: string
-  value: string
-  options?: CookieOptions
-}
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
@@ -21,7 +14,7 @@ export async function updateSession(request: NextRequest) {
         getAll() {
           return request.cookies.getAll()
         },
-        setAll(cookiesToSet: CookieToSet[]) {
+        setAll(cookiesToSet: any[]) {
           cookiesToSet.forEach(({ name, value }) =>
             request.cookies.set(name, value)
           )
